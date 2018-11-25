1edd15a124ce19680a0484455196c13ee471ade6
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2018-11-25 18:43:19 +0200

    Ecran cont personal student
    S-a futut validarea pe mail si parola inregistrare stud
    
    _|_ <3

--- app/src/main/java/com/example/catalinadinu/androidquiz/ConectareStudent.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/ConectareStudent.java
@@ -1,13 +1,34 @@
 package com.example.catalinadinu.androidquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConectareStudent extends Activity {
+
+    private Button conectareStud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conectare_student);
+
+
+        conectareStud = findViewById(R.id.id_butonConectareStud);
+
     }
+
+    public void conectareStudent(View view){
+        conectareStud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentConectareStud = new Intent(ConectareStudent.this, ContStudent.class);
+                startActivityForResult(intentConectareStud, 5);
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/catalinadinu/androidquiz/InregistrareProfilStudent.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/InregistrareProfilStudent.java
@@ -25,62 +25,83 @@ public class InregistrareProfilStudent extends AppCompatActivity {
     public UtilizatorStudent utilStud;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inregistrare_profil_student);
 
-        nume=findViewById(R.id.id_numeStudI);
-        prenume=findViewById(R.id.id_prenumeStudI);
-        email=findViewById(R.id.id_mailStudI);
-        parola=findViewById(R.id.id_parolaStudI);
-        confirmaParola=findViewById(R.id.id_confirmParolaStudI);
-        creeazaCont=findViewById(R.id.id_butonInregistreazaStud);
+        nume = findViewById(R.id.id_numeStudI);
+        prenume = findViewById(R.id.id_prenumeStudI);
+        email = findViewById(R.id.id_mailStudI);
+        parola = findViewById(R.id.id_parolaStudI);
+        confirmaParola = findViewById(R.id.id_confirmParolaStudI);
+        creeazaCont = findViewById(R.id.id_butonInregistreazaStud);
         //radioGroupTip = findViewById(R.id.id_radioGrup);
 
     }
 
-    public void creeazaContStudent(View view)
-    {
-        if(nume!=null && prenume!=null && email!=null && parola!=null && confirmaParola != null)
+//    public void validareEmail() {
+//        if (!(emailValue.matches(emailPattern))) {
+//            Toast.makeText(getApplicationContext(), "Email invalid!", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
+        public void creeazaContStudent (View view)
         {
-            if("".equals(nume.getText().toString()) || "".equals(prenume.getText().toString()) ||
-                    "".equals(email.getText().toString()) || "".equals(parola.getText().toString()) ||
-                    "".equals(confirmaParola.getText().toString()))
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Eroare");
-                builder.setMessage("Toate câmpurile sunt obligatorii!");
-                builder.setPositiveButton("OK", null);
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-            else
-            {
-                //String radioValue = ((RadioButton)findViewById(radioGroupTip.getCheckedRadioButtonId())).getText().toString();
-                //UtilizatorStudent utilizator= new UtilizatorStudent();
-                if(confirmaParola.equals(parola)){
-                    utilStud = new UtilizatorStudent(nume.getText().toString(),prenume.getText().toString(), email.getText().toString(),
+            String emailValue = email.getText().toString().trim();
+            String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+            if (nume != null && prenume != null && email != null && parola != null && confirmaParola != null) {
+                if ("".equals(nume.getText().toString()) || "".equals(prenume.getText().toString()) ||
+                        "".equals(email.getText().toString()) || "".equals(parola.getText().toString()) ||
+                        "".equals(confirmaParola.getText().toString())) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Eroare");
+                    builder.setMessage("Toate câmpurile sunt obligatorii!");
+                    builder.setPositiveButton("OK", null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else if ((confirmaParola.equals(parola)) && (emailValue.matches(emailPattern))) {
+                    utilStud = new UtilizatorStudent(nume.getText().toString(), prenume.getText().toString(), email.getText().toString(),
                             parola.getText().toString(), confirmaParola.getText().toString());
-                    //Toast.makeText(InregistrareProfilStudent.this, utilizator.toString(), Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(InregistrareProfilStudent.this, "Parola nu coincide!", Toast.LENGTH_SHORT).show();
+
+                    creeazaCont.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intentConectare = new Intent(InregistrareProfilStudent.this, ConectareStudent.class);
+                           startActivityForResult(intentConectare, 4);                 }
+                 });
+                } else if (!(confirmaParola.equals(parola)) || !(emailValue.matches(emailPattern))){
+                    Toast.makeText(InregistrareProfilStudent.this, "Email-ul nu e introdus corect sau parola nu coincide!", Toast.LENGTH_SHORT).show();
                 }
 
-                //Toast.makeText(Inregistrare.this, utilizator.toString(), Toast.LENGTH_SHORT).show();
-//                Intent intent = new Intent();
-//                intent.putExtra("util", utilizator);
-//                setResult(RESULT_OK, intent);
-//                finish();
-                creeazaCont.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intentConectare = new Intent(InregistrareProfilStudent.this, ConectareStudent.class);
-                        startActivityForResult(intentConectare, 4);
-                    }
-                });
             }
+
+//                if (confirmaParola.equals(parola)) {
+////                    utilStud = new UtilizatorStudent(nume.getText().toString(), prenume.getText().toString(), email.getText().toString(),
+////                            parola.getText().toString(), confirmaParola.getText().toString());
+//                    //Toast.makeText(InregistrareProfilStudent.this, utilizator.toString(), Toast.LENGTH_SHORT).show();
+//                } else {
+////                    Toast.makeText(InregistrareProfilStudent.this, "Parola nu coincide!", Toast.LENGTH_SHORT).show();
+//                }
+//                if (!(emailValue.matches(emailPattern))) {
+//                    Toast.makeText(getApplicationContext(), "Email invalid!", Toast.LENGTH_SHORT).show();
+//                }
+
+
+
+
+            creeazaCont.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentConectare = new Intent(InregistrareProfilStudent.this, ConectareStudent.class);
+                    startActivityForResult(intentConectare, 4);
+                }
+            });
         }
     }
-}
+
+
+
+
