e2f8e98905a6f8d16fc82882cea0606342fd924c
Unknown <andreeadanalache5@gmail.com>
2018-11-28 20:10:44 +0200

    Butonul de pe feedback aplicatie - toast scor oferit + inapoi in main
    pwp

--- app/src/main/java/com/example/catalinadinu/androidquiz/FeedbackAplicatie.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/FeedbackAplicatie.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class FeedbackAplicatie extends Activity {
 
     private ImageButton butonBackfeedbackAplicatie;
+    private RatingBar ratingBarFDBAplicatie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,27 @@ public class FeedbackAplicatie extends Activity {
         setContentView(R.layout.activity_feedback_aplicatie);
 
         butonBackfeedbackAplicatie = findViewById(R.id.feedbackBackButton);
-    }
+        ratingBarFDBAplicatie = findViewById(R.id.ratingBarFDBAplicatie);
 
-    public void backToMain(View view){
+
+        butonBackfeedbackAplicatie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FeedbackAplicatie.this,
+                String.valueOf("Scorul oferit aplicației: "+ratingBarFDBAplicatie.getRating()), Toast.LENGTH_LONG).show();
+
         Intent intent = new Intent(FeedbackAplicatie.this, MainActivity.class);
         startActivityForResult(intent, 0);
+            }
+        });
     }
+
+//    public void backToMain(View view){
+//
+//        Toast.makeText(FeedbackAplicatie.this,
+//                String.valueOf("Scorul oferit aplicației: "+ratingBarFDBAplicatie.getRating()), Toast.LENGTH_LONG).show();
+//
+//        Intent intent = new Intent(FeedbackAplicatie.this, MainActivity.class);
+//        startActivityForResult(intent, 0);
+//    }
 }
--- app/src/main/java/com/example/catalinadinu/androidquiz/VizualizareTest.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/VizualizareTest.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class VizualizareTest extends Activity {
 
+    //va fi legat de un item din listview cand acesta va fi populat
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
