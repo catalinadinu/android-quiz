96d1ec22e152ad55f291549d2667144a6f49616a
Alexandra Dumitru <alexandra_florentina22@yahoo.com>
2018-12-04 14:16:13 +0200

    Buton partajare pe activitatea de vizualizare test + intent catre activitatea de partajare
    Face fata design si pe activitatea cu codul 16 cand se intoarce.

--- app/src/main/java/com/example/catalinadinu/androidquiz/Contact.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/Contact.java
@@ -2,8 +2,10 @@ package com.example.catalinadinu.androidquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Contact extends Activity {
+    private Button deschideMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
