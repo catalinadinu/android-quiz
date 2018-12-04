e73f443e27dcf90d4c9e0bbc3694ad94e9093f00
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2018-12-04 22:50:10 +0200

    Butoane parasire cont -> trimit in Main Activity

--- app/src/main/java/com/example/catalinadinu/androidquiz/setariContProfesor.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/setariContProfesor.java
@@ -1,7 +1,9 @@
 package com.example.catalinadinu.androidquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class setariContProfesor extends Activity {
 
@@ -9,5 +11,10 @@ public class setariContProfesor extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setari_cont_profesor);
+    }
+
+    public void deconectareProfesor(View view){
+        Intent intentDeconectareProfesor = new Intent(setariContProfesor.this, MainActivity.class);
+        startActivityForResult(intentDeconectareProfesor, 18);
     }
 }
--- app/src/main/java/com/example/catalinadinu/androidquiz/setariContStudent.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/setariContStudent.java
@@ -1,7 +1,9 @@
 package com.example.catalinadinu.androidquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class setariContStudent extends Activity {
 
@@ -9,5 +11,10 @@ public class setariContStudent extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setari_cont_student);
+    }
+
+    public void deconectareStudent(View view){
+        Intent intentDeconectareStudent = new Intent(setariContStudent.this, MainActivity.class);
+        startActivityForResult(intentDeconectareStudent, 18);
     }
 }
