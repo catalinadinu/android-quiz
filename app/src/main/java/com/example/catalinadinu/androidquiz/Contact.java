260eb9ed6b92db8e4ee4126afa6c6b6ec18e6a7f
Alexandra Dumitru <alexandra_florentina22@yahoo.com>
2018-12-05 11:42:40 +0200

    Buton vizualizare site + redirectionare catre site csie
    Bula fetele! Suntem pe bines

--- app/src/main/java/com/example/catalinadinu/androidquiz/Contact.java
+++ app/src/main/java/com/example/catalinadinu/androidquiz/Contact.java
@@ -1,15 +1,34 @@
 package com.example.catalinadinu.androidquiz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Contact extends Activity {
-    private Button deschideMaps;
+    private ImageButton deschideMaps;
+    private ImageButton veziSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        veziSite = findViewById(R.id.veziSite);
+
+        veziSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicitIntent =
+                        new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://csie.ase.ro/"));
+                startActivity(implicitIntent);
+            }
+        });
     }
+
+
 }
